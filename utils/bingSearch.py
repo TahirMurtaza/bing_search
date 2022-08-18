aae619c0d8d10fe47fbc08d88d25d82fe3559ccd
TahirMurtaza <--global>
2022-08-19 01:11:08 +0500

    proxy settings

--- utils/bingSearch.py
+++ utils/bingSearch.py
@@ -2,13 +2,18 @@ from bs4 import BeautifulSoup
 import requests_async as requests
 from utils.fake_user import get_fake_user_agent
 import urllib.parse
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 async def customsearch(searchquery):
-    proxy = {'https': 'http://user-lyneadmin:lyneadmin@gate.dc.smartproxy.com:20000'}
+    username = os.getenv('username')
+    password = os.getenv('password')
+    PROXY_RACK_DNS = os.getenv('PROXY_RACK_DNS')
+    proxy = {"https":"http://{}:{}@{}".format(username, password, PROXY_RACK_DNS)}
     query='"%s" "%s" site:linkedin.com' % (searchquery['title'], searchquery['company'])
     safe_querystring = urllib.parse.quote_plus(query)
     url = 'https://www.bing.com/search?q='+safe_querystring
-    print(url)
     headers = {'User-Agent': get_fake_user_agent()}
     response = await requests.get(url, proxies=proxy, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
